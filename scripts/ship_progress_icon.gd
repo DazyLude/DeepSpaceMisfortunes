6f93beafa611e5a368905a6bf4fa285bcba59aac
SearchingFox <n3tstrang3r@yandex.ru>
2025-04-07 04:33:03 +0300

    Fix progress bar overload

--- scripts/ship_progress_icon.gd
+++ scripts/ship_progress_icon.gd
@@ -9,11 +9,12 @@ var tween = null;
 func _advance(_dummy) -> void:
 	if tween != null:
 		tween.kill();
-	
-	var progress = GameState.travel_distance / GameState.TRAVEL_GOAL;
 	tween = get_tree().create_tween();
-	tween.tween_property(self, "position", \
-		Vector2(start_x + progress_bar_len * progress, 5), 1) \
+	
+	var distance = min(GameState.TRAVEL_GOAL, GameState.travel_distance);
+	var progress = distance / GameState.TRAVEL_GOAL;
+	tween.tween_property(self, "position",
+		Vector2(start_x + progress_bar_len * progress, 5), 1.0)\
 		.set_trans(Tween.TRANS_QUAD);
 
 

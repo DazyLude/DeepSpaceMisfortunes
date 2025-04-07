2b08e19086e8b94fcfb8665ce37c2a5c21d7f0f8
DazyLude <36658009+DazyLude@users.noreply.github.com>
2025-04-07 18:24:03 +0400

    progress bar prediction

--- scripts/ship_progress_icon.gd
+++ scripts/ship_progress_icon.gd
@@ -1,5 +1,6 @@
 extends TextureRect
 
+@export var predict : bool = false;
 
 @onready var start_x = self.position.x;
 const progress_bar_len = 400;
@@ -11,7 +12,13 @@ func _advance(_dummy) -> void:
 		tween.kill();
 	tween = get_tree().create_tween();
 	
-	var distance = min(GameState.TRAVEL_GOAL, GameState.travel_distance);
+	var distance;
+	
+	if predict:
+		distance = min(GameState.TRAVEL_GOAL, GameState.travel_distance + GameState.get_speed());
+	else:
+		distance = min(GameState.TRAVEL_GOAL, GameState.travel_distance);
+	
 	var progress = distance / GameState.TRAVEL_GOAL;
 	tween.tween_property(self, "position",
 		Vector2(start_x + progress_bar_len * progress, 5), 1.0)\
